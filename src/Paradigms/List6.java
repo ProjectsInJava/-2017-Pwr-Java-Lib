e0da8a5a62a7063d5d8a4356f80dfb21eeee7b01
trunksBT <korytko07@gmail.com>
2017-11-19 13:31:24 +0100

    Exercises 6, Java, List6_3, Compare primitive and packaged types

--- src/Paradigms/List6.java
+++ src/Paradigms/List6.java
@@ -1,6 +1,23 @@
 package Paradigms;
 
 public class List6 {
+    static boolean isEqualSimpleType(int argLeft, int argRight)
+    {
+        return argLeft == argRight;
+    }
+    static boolean isEqualPackagedType(Integer argLeft, Integer argRight)
+    {
+        return argLeft == argRight;
+    }
+
+    public static void Exercise3()
+    {
+        System.out.println("*********** Comparator when primitive types as args ***********");
+        System.out.println(isEqualSimpleType(500,500));
+
+        System.out.println("*********** Comparator when packaged types as args ***********");
+        System.out.println(isEqualPackagedType(500,500));
+    }
     public static void Exercise4()
     {
         System.out.println("*********** Flyweight ***********");
@@ -11,6 +28,7 @@ public class List6 {
         System.out.println("Value compare");
         System.out.println(s1.equals(s2));
         String s3 = new String("foo");
+
         System.out.println("*********** Explicit create new object ***********");
         System.out.println("Reference compare");
         System.out.println(s1 == s3);
@@ -20,6 +38,7 @@ public class List6 {
 
     public static void main (String[] args) throws java.lang.Exception
     {
+        Exercise3();
 //        Exercise4();
     }
 }
