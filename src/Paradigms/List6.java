bc5b2a505271a563a61388e17be1aa1e09a3bdd8
trunksBT <korytko07@gmail.com>
2017-11-19 16:01:56 +0100

    Exercises 6, Java, List6_5, Access to table by foreach local variable and ArrayAccessExpression

--- src/Paradigms/List6.java
+++ src/Paradigms/List6.java
@@ -1,5 +1,8 @@
 package Paradigms;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class List6 {
     static boolean isEqualSimpleType(int argLeft, int argRight)
     {
@@ -36,9 +39,37 @@ public class List6 {
         System.out.println(s1.equals(s3));
     }
 
+    public static void Exercise5()
+    {
+        System.out.println("*********** For Each, Range Based Loop ***********");
+        System.out.println("*********** Modyfing vals by local variable ******");
+        int[] ints = {1,2,3};
+        for (int it : ints) {
+            it = 0;
+            System.out.print(it);
+        }
+        System.out.println("\n*********** Check if modified, should be 0 everywhere ******");
+        for (int it : ints) {
+            System.out.print(it);
+        }
+
+        System.out.println("\n*********** For and accessing with operator[] ***********");
+        System.out.println("*********** Modyfing vals by Array Access Expression ******");
+        int[] ints2 = ints;
+        for (int i = 0; i < ints2.length; i++) {
+            ints2[i] = 0;
+            System.out.print(ints2[i]);
+        }
+        System.out.println("\n*********** Check if modified, should be 0 everywhere ******");
+        for (int i : ints) {
+            System.out.print(i);
+        }
+    }
+
     public static void main (String[] args) throws java.lang.Exception
     {
-        Exercise3();
+//        Exercise3();
 //        Exercise4();
+        Exercise5();
     }
 }
