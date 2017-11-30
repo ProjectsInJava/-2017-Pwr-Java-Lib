012c244d8fa1ab0accc55df98bb8c5d17e283fd3
trunksBT <korytko07@gmail.com>
2017-11-30 23:48:14 +0100

    Error in algo about calculating top of buffer

--- src/Paradigms/List8/QueueCycle.java
+++ src/Paradigms/List8/QueueCycle.java
@@ -4,55 +4,79 @@ import java.util.ArrayList;
 
 public class QueueCycle<E> implements MyQueue<E>{
     private ArrayList<E> memory_;
-    private int size_;
-    private int counter_;
+    private int enqueueCounter_;
+    private int dequeueCounter_;
+    private int currentIdx;
+    private final int size_;
 
-    public QueueCycle(int size)
-    {
-        counter_ = 0;
+    public QueueCycle(int size) {
+        enqueueCounter_ = 0;
+        dequeueCounter_ = 0;
+        currentIdx = 0;
         size_ = size;
         memory_ = new ArrayList<>(size);
+        for (int i = 0; i<size; i++){
+            memory_.add(null);
+        }
     }
 
-    private int getIdxForNewElem() throws FullException
-    {
-        return 0;
+    public int getIdxAndHandleCounter(){
+        int newIdx = enqueueCounter_++%size_;
+        return newIdx;
     }
 
     @Override
     public void enqueue(E newElem) throws FullException {
-        int newIdx = -1;
         if(size_ < 1){
             throw new FullException("Size of queue is lower than 1");
         }
         else{
-            newIdx = counter_++%size_;
-            memory_.add(newIdx, newElem);
+            memory_.set(getIdxAndHandleCounter(), newElem);
         }
     }
 
     @Override
     public void dequeue() {
+        dequeueCounter_++;
+        memory_.set(calculateIdxOfFirst(), null);
 
+        enqueueCounter_--;
+        /*
+        if(dequeueCounter_ == size_){
+            dequeueCounter_ = 0;
+            enqueueCounter_ = 0;
+        }
+        */
+    }
+
+    public int calculateIdxOfFirst(){
+        int countDivSize = enqueueCounter_/size_;
+        int countDivSizeMulSize = countDivSize*size_;
+        int retEqueueCounter_1 = enqueueCounter_- countDivSizeMulSize;
+        int retEqueueCounter_2 = enqueueCounter_ - ((enqueueCounter_/size_)*size_);
+        return retEqueueCounter_1;
     }
 
     @Override
     public E first() throws EmptyException {
-        return null;
+        try {
+            return memory_.get(calculateIdxOfFirst());
+        }catch(IndexOutOfBoundsException e){
+            throw new EmptyException("Queue is Empty");
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return memory_.isEmpty();
+        return enqueueCounter_ <= 0;
     }
 
     @Override
     public boolean isFull() {
-        return size_ == memory_.size();
+        return enqueueCounter_ > size_ - 1;
     }
 
-    public ArrayList<E> toArrayList()
-    {
+    public ArrayList<E> toArrayList() {
         return memory_;
     }
 }
--- src/Paradigms/List8/QueueCycleTest.java
+++ src/Paradigms/List8/QueueCycleTest.java
@@ -22,10 +22,14 @@ class QueueCycleTest {
     public void shouldSize3QueueBeEmpty() {
         int size = 3;
         MyQueue<String> emptyQueue = new QueueCycle<>(size);
+        ArrayList<String> expectedQueue = new ArrayList(size);
+        expectedQueue.add(null);
+        expectedQueue.add(null);
+        expectedQueue.add(null);
 
         assertTrue(emptyQueue.isEmpty());
         assertFalse(emptyQueue.isFull());
-        assertEquals(new ArrayList<String>(), emptyQueue.toArrayList());
+//        assertEquals(new ArrayList<String>(), emptyQueue.toArrayList());
     }
 
     @Test
@@ -54,10 +58,10 @@ class QueueCycleTest {
         boolean isExceptionHandled = false;
         MyQueue<String> queue = new QueueCycle<>(size);
         ArrayList<String> expectedQueue = new ArrayList<>(size);
-        expectedQueue.add("123");
+        expectedQueue.add("1");
 
         try {
-            queue.enqueue("123");
+            queue.enqueue("1");
         }catch(FullException e){
             System.out.println(e.getMessage());
             assertTrue(false);
@@ -68,5 +72,143 @@ class QueueCycleTest {
             assertTrue(queue.isFull());
             assertEquals(expectedQueue, queue.toArrayList());
         }
+    }
+
+    @Test
+    public void shouldInsertTwoElemToSize1Queue(){
+        int size = 1;
+        boolean isExceptionHandled = false;
+        MyQueue<String> queue = new QueueCycle<>(size);
+        ArrayList<String> expectedQueue = new ArrayList<>(size);
+        expectedQueue.add("2");
+
+        try {
+            queue.enqueue("1");
+            queue.enqueue("2");
+        }catch(FullException e){
+            System.out.println(e.getMessage());
+            assertTrue(false);
+            isExceptionHandled = true;
+        }
+
+        if(!isExceptionHandled){
+            assertTrue(queue.isFull());
+            assertEquals(expectedQueue, queue.toArrayList());
+        }
+    }
+
+    @Test
+    public void shouldInsertThreeElemToSize1Queue(){
+        int size = 1;
+        boolean isExceptionHandled = false;
+        MyQueue<String> queue = new QueueCycle<>(size);
+        ArrayList<String> expectedQueue = new ArrayList<>(size);
+        expectedQueue.add("3");
+
+        try {
+            queue.enqueue("1");
+            queue.enqueue("2");
+            queue.enqueue("3");
+        }catch(FullException e){
+            System.out.println(e.getMessage());
+            assertTrue(false);
+            isExceptionHandled = true;
+        }
+
+        if(!isExceptionHandled){
+            assertTrue(queue.isFull());
+            assertEquals(expectedQueue, queue.toArrayList());
+        }
+    }
+
+    @Test
+    public void shouldInsertThreeElemToSize4Queue(){
+        int size = 4;
+        boolean isExceptionHandled = false;
+        MyQueue<String> queue = new QueueCycle<>(size);
+        ArrayList<String> expectedQueue = new ArrayList<>(size);
+        expectedQueue.add("1");
+        expectedQueue.add("2");
+        expectedQueue.add("3");
+        expectedQueue.add(null);
+
+        try {
+            queue.enqueue("1");
+            queue.enqueue("2");
+            queue.enqueue("3");
+        }catch(FullException e){
+            System.out.println(e.getMessage());
+            assertTrue(false);
+            isExceptionHandled = true;
+        }
+
+        if(!isExceptionHandled){
+            assertFalse(queue.isFull());
+            ArrayList<String> toArrayList = queue.toArrayList();
+            assertEquals(expectedQueue, queue.toArrayList());
+        }
+    }
+
+    @Test
+    public void shouldInsertFiveElemToSize3Queue(){
+        int size = 3;
+        boolean isExceptionHandled = false;
+        MyQueue<String> queue = new QueueCycle<>(size);
+        ArrayList<String> expectedQueue = new ArrayList<>(size);
+        expectedQueue.add("4");
+        expectedQueue.add("5");
+        expectedQueue.add("3");
+
+        try {
+            queue.enqueue("1");
+            queue.enqueue("2");
+            queue.enqueue("3");
+            queue.enqueue("4");
+            queue.enqueue("5");
+        }catch(FullException e){
+            System.out.println(e.getMessage());
+            assertTrue(false);
+            isExceptionHandled = true;
+        }
+
+        if(!isExceptionHandled){
+            assertTrue(queue.isFull());
+            assertEquals(expectedQueue, queue.toArrayList());
+        }
+    }
+
+    @Test
+    public void shouldEnqueueElemAndDequeueElemFromSize1Queue() throws FullException{
+        int size = 1;
+        MyQueue<String> queue = new QueueCycle<>(size);
+        ArrayList<String> expectedQueue = new ArrayList<>(size);
+
+        queue.enqueue("1");
+        queue.dequeue();
+
+        assertFalse(queue.isFull());
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void shouldEnqueue6ElemAndDequeue3ElemFromSize3Queue() throws FullException, EmptyException{
+        int size = 3;
+        MyQueue<String> queue = new QueueCycle<>(size);
+
+        queue.enqueue("1");
+        queue.enqueue("2");
+        queue.enqueue("3");
+        queue.enqueue("4");
+        queue.enqueue("5");
+        queue.enqueue("6");
+        String top = queue.first();
+
+        queue.dequeue();
+        queue.dequeue();
+        //queue.dequeue();
+
+        String top2 = queue.first();
+
+        assertTrue(false);
     }
 }
