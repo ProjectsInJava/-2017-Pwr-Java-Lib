221188939a17bff5e4fbb4b76460258294ad8be5
trunksBT <korytko07@gmail.com>
2018-01-06 16:43:49 +0100

    Exercises 10, Java, List11_3; Rename variables

--- src/Paradigms/List11/BoundedBuffer/BoundedBuffer.java
+++ src/Paradigms/List11/BoundedBuffer/BoundedBuffer.java
@@ -1,51 +1,50 @@
 package Paradigms.List11.BoundedBuffer;
 
 public class BoundedBuffer implements Consume, Produce {
-
     public BoundedBuffer(int N){
-        this.N_ = N;
-        elems_ = new int[N_];
+        this.SIZE_ = N;
+        elems_ = new int[SIZE_];
     }
 
     @Override
     public synchronized void put(int inVal) {
-        while (n_ >= N_){
+        while (elemsCounter_ >= SIZE_){
             try{
-                System.out.println(Thread.currentThread().getName() + " waiting with " + inVal);
+                System.out.println(Thread.currentThread().getName() + " waiting with put the " + inVal);
                 wait();
             } catch (InterruptedException e){ System.out.println(e); }
 
         }
-        elems_[in_] = inVal;
-        in_ = (in_ + 1) % N_;
-        n_ += 1;
+        elems_[idxToPut_] = inVal;
+        idxToPut_ = (idxToPut_ + 1) % SIZE_;
+        elemsCounter_ += 1;
         System.out.println(Thread.currentThread().getName() + " produced: " + inVal);
-        if (n_ == 1){
+        if (elemsCounter_ == 1){
             notifyAll();
         }
     }
 
     @Override
     public synchronized int take() {
-        while (n_ == 0){
+        while (elemsCounter_ == 0){
             try {
-                System.out.println(Thread.currentThread().getName() + " waiting");
+                System.out.println(Thread.currentThread().getName() + " waiting on take");
                 wait();
             } catch( InterruptedException e){ System.out.print(e); }
         }
-        int x = elems_[out_];
-        out_ = (out_ + 1) % N_;
-        n_ -= 1;
+        int x = elems_[idxToTake_];
+        idxToTake_ = (idxToTake_ + 1) % SIZE_;
+        elemsCounter_ -= 1;
         System.out.println(Thread.currentThread().getName()+ " consuming: " + x);
-        if (n_ == N_-1){
+        if (elemsCounter_ == SIZE_-1){
             notifyAll();
         }
         return x;
     }
 
     private int[] elems_;
-    private int in_ = 0;
-    private int out_ = 0;
-    private int n_ = 0;
-    private final int N_;
+    private int idxToPut_ = 0;
+    private int idxToTake_ = 0;
+    private int elemsCounter_ = 0;
+    private final int SIZE_;
 }
