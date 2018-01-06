ac099f604bfdb86148a243157437648bc2f152be
trunksBT <korytko07@gmail.com>
2018-01-06 16:29:18 +0100

    Exercises 10, Java, List11_3; Rewrite fully

---
+++ src/Paradigms/List11/BoundedBuffer/ProdCons.java
@@ -0,0 +1,23 @@
+package Paradigms.List11.BoundedBuffer;
+
+public class ProdCons {
+    public static void main(String args[]){
+        final int bufferCapacity = 5;
+        final int numberOfConsumedElems = 3;
+        final int numberOfProducedElems = 3;
+        BoundedBuffer buf = new BoundedBuffer(bufferCapacity);
+        Consumer[] cons = new Consumer[numberOfConsumedElems];
+        Producer[] prods = new Producer[numberOfProducedElems];
+
+        for (int i = 0; i < numberOfProducedElems; i++){
+            prods[i] = new Producer("Producer " + (i+1), buf);
+            prods[i].start();
+        }
+
+        for (int i = 0; i < numberOfConsumedElems; i++){
+            cons[i] = new Consumer("Consumer " + (i+1), buf);
+            cons[i].start();
+        }
+
+    }
+}
